d7e3e3ccc32ec6ece1a08952eddbef2b9d5c5540
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-02-12 01:13:32 +0630

    'fixed-error'

--- src/__init__.py
+++ src/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from src.router.routes import api, web
 
-app = Flask(__name__, template_folder="./src/templates")
+app = Flask(__name__, template_folder="./templates")
 app.secret_key = "oOUP0oHOQKHnmZHbXT8IxQ99Ml4Q2ZMv"
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(web)
+
 
 def create_app():
     return app
