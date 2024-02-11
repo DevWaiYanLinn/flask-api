b01eb7fbbc41460c657d2851cc5f9a42a53fc625
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-02-11 20:29:04 +0630

    fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.router.routes import api, web
 
 app = Flask(__name__, template_folder="./src/templates")
 app.secret_key = "oOUP0oHOQKHnmZHbXT8IxQ99Ml4Q2ZMv"
-cors = CORS(app, resources={r"*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(web)
