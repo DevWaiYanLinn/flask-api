9d0c8a7627a5123bfa1822f702aa386954e2a9c9
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-02-11 20:58:07 +0630

    test

--- src/controllers/predict_controller.py
+++ src/controllers/predict_controller.py
@@ -13,22 +13,23 @@ def allowed_file(filename):
 
 @predicts.route("/", methods=["POST"])
 def upload_file():
-    try:
-        if "file" not in request.files:
-            raise ValueError("File not found in the request.")
+    return ''
+    # try:
+    #     if "file" not in request.files:
+    #         raise ValueError("File not found in the request.")
 
-        file = request.files["file"]
+    #     file = request.files["file"]
 
-        if file.filename == "":
-            raise ValueError("Empty filename in the request.")
+    #     if file.filename == "":
+    #         raise ValueError("Empty filename in the request.")
 
-        if file and allowed_file(file.filename):
-            image = file.read()
-            image_data = np.frombuffer(image, np.uint8)
-            results = test_pipeline(image_data)
-            return jsonify(results), 200
-        else:
-            raise ValueError("Invalid file type.")
+    #     if file and allowed_file(file.filename):
+    #         image = file.read()
+    #         image_data = np.frombuffer(image, np.uint8)
+    #         results = test_pipeline(image_data)
+    #         return jsonify(results), 200
+    #     else:
+    #         raise ValueError("Invalid file type.")
 
-    except Exception as e:
-        return f"Error processing file: {str(e)}", 500
+    # except Exception as e:
+    #     return f"Error processing file: {str(e)}", 500
